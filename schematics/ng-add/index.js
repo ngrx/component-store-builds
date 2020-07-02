2f68db5f4ead38efbff7724f6131103bb0360202
GitHub <noreply@github.com>
2020-07-02 12:09:11 +0000

    af7b2cc2 feat(component-store): add ng-add and ng-update schematics (#2598)

---
+++ schematics/ng-add/index.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+var schematics_1 = require("@angular-devkit/schematics");
+var tasks_1 = require("@angular-devkit/schematics/tasks");
+var schematics_core_1 = require("@ngrx/component-store/schematics-core");
+function addModuleToPackageJson() {
+    return function (host, context) {
+        schematics_core_1.addPackageToPackageJson(host, 'dependencies', '@ngrx/component-store', schematics_core_1.platformVersion);
+        context.addTask(new tasks_1.NodePackageInstallTask());
+        return host;
+    };
+}
+function default_1(options) {
+    return function (host, context) {
+        return schematics_1.chain([
+            options && options.skipPackageJson ? schematics_1.noop() : addModuleToPackageJson(),
+        ])(host, context);
+    };
+}
+exports["default"] = default_1;
+//# sourceMappingURL=index.js.map
