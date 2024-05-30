0ef8a92db1356eb86f3ad9e043700c80c181fbf5
GitHub <noreply@github.com>
2024-05-30 20:07:22 +0000

    1a1b6df4 feat(effects): remove concatLatestFrom operator (#4367)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,2 @@
-import * as operators from '@ngrx/operators';
 export * from './component-store';
 export { provideComponentStore, OnStateInit, OnStoreInit, } from './lifecycle_hooks';
-/**
- * @deprecated Use `tapResponse` from `@ngrx/operators` instead.
- */
-export declare const tapResponse: typeof operators.tapResponse;
