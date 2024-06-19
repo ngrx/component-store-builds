4bdafaff3dafdbe788561f9cf4dc2b0292fe11f5
GitHub <noreply@github.com>
2024-06-19 17:18:22 +0000

    a15d53eb fix(component-store): resolve issues in migration script to v18 (#4403)

--- migrations/18_0_0-beta/index.js
+++ migrations/18_0_0-beta/index.js
@@ -32,23 +32,33 @@ var schematics_core_1 = require("../../schematics-core");
 var change_1 = require("../../schematics-core/utility/change");
 function migrateTapResponseImport() {
     return function (tree, ctx) {
-        var changes = [];
         (0, schematics_core_1.addPackageToPackageJson)(tree, 'dependencies', '@ngrx/operators', '^18.0.0');
         (0, schematics_core_1.visitTSSourceFiles)(tree, function (sourceFile) {
             var _a;
             var importDeclarations = new Array();
             getImportDeclarations(sourceFile, importDeclarations);
-            var componentStoreImportsAndDeclaration = importDeclarations
+            var componentStoreImportsAndDeclarations = importDeclarations
                 .map(function (componentStoreImportDeclaration) {
                 var componentStoreImports = getComponentStoreNamedBinding(componentStoreImportDeclaration);
                 if (componentStoreImports) {
-                    return { componentStoreImports: componentStoreImports, componentStoreImportDeclaration: componentStoreImportDeclaration };
+                    if (componentStoreImports.elements.some(function (element) { return element.name.getText() === 'tapResponse'; })) {
+                        return { componentStoreImports: componentStoreImports, componentStoreImportDeclaration: componentStoreImportDeclaration };
+                    }
+                    return undefined;
                 }
                 else {
                     return undefined;
                 }
             })
-                .find(Boolean);
+                .filter(Boolean);
+            if (componentStoreImportsAndDeclarations.length === 0) {
+                return;
+            }
+            else if (componentStoreImportsAndDeclarations.length > 1) {
+                ctx.logger.info('[@ngrx/component-store] Skipping because of multiple `tapResponse` imports');
+                return;
+            }
+            var _b = __read(componentStoreImportsAndDeclarations, 1), componentStoreImportsAndDeclaration = _b[0];
             if (!componentStoreImportsAndDeclaration) {
                 return;
             }
@@ -60,6 +70,7 @@ function migrateTapResponseImport() {
                 .filter(function (element) { return element.name.getText() !== 'tapResponse'; })
                 .map(function (element) { return element.name.getText(); })
                 .join(', ');
+            var changes = [];
             // Remove `tapResponse` from @ngrx/component-store and leave the other imports
             if (otherComponentStoreImports) {
                 changes.push((0, schematics_core_1.createReplaceChange)(sourceFile, componentStoreImportDeclaration, componentStoreImportDeclaration.getText(), "import { ".concat(otherComponentStoreImports, " } from '@ngrx/component-store';")));
@@ -84,7 +95,8 @@ function migrateTapResponseImport() {
             if (!importAppendedInExistingDeclaration) {
                 // Add new @ngrx/operators import line
                 var newOperatorsImport = "import { tapResponse } from '@ngrx/operators';";
-                changes.push(new schematics_core_1.InsertChange(sourceFile.fileName, componentStoreImportDeclaration.getEnd() + 1, "".concat(newOperatorsImport, "\n")));
+                changes.push(new schematics_core_1.InsertChange(sourceFile.fileName, componentStoreImportDeclaration.getEnd() + 1, "".concat(newOperatorsImport, "\n") // not os-independent for snapshot tests
+                ));
             }
             (0, schematics_core_1.commitChanges)(tree, sourceFile.fileName, changes);
             if (changes.length) {
